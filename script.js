4677beb1d25b0509477a4df5aaed44a64824e18d
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-04-25 23:02:51 +0100

    added images as backgrounds, and buttons

---
+++ script.js
@@ -0,0 +1,20 @@
+let menuToggle = document.getElementById("menu-button")
+let menuBar = document.getElementById("menu-bar")
+
+
+menuToggle.addEventListener("click", openMenu)
+
+
+function openMenu() {
+    if(menuBar.style.display === "none") {
+      menuBar.style.display = "flex";
+      setTimeout(() => {
+        menuBar.classList.add("open");
+      }, 50); // delay to allow display to change before animation starts
+    } else {
+      menuBar.classList.remove("open");
+      setTimeout(() => {
+        menuBar.style.display = "none";
+      }, 500); // duration of the animation
+    }
+  }
