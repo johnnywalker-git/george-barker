2b28e3e6462b48d07d16e17bbbfec313888cb4e4
Johnathan Walker <91965220+johnnywalker-git@users.noreply.github.com>
2023-05-08 10:31:08 +0100

    Working buttons

--- script.js
+++ script.js
@@ -3,20 +3,13 @@ let menuBar = document.getElementById("menu-bar")
 
 
 menuToggle.addEventListener("click", openMenu)
-menuToggle.addEventListener("touchstart", openMenu);
-
 
 
 function openMenu() {
-    if(menuBar.style.display === "none") {
-      menuBar.style.display = "flex";
-      setTimeout(() => {
-        menuBar.classList.add("open");
-      }, 50); // delay to allow display to change before animation starts
-    } else {
-      menuBar.classList.remove("open");
-      setTimeout(() => {
-        menuBar.style.display = "none";
-      }, 500); // duration of the animation
+  if(menuBar.style.display === "none") {
+    menuBar.style.display = "flex";
+  } else {
+    menuBar.style.display = "none";
+    console.log("Just been clicked again");
+  }  
     }
-  }
